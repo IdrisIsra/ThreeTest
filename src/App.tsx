c022b42c84f29600ed5afd9fe482bc8f42170442
IdrisIsra <israpilov.idris@gmail.com>
2022-10-23 20:19:30 +0300

    git hiccup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import * as THREE from "three";
+import React, { Suspense, useRef, useState } from "react";
+import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Model } from "./Model";
+import "./index.css";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div id="canvas-container" style={{ height: "100vh" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Suspense fallback={null}>
+          <Model position={[0, -1, 0]} />
+          <OrbitControls />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,7 @@
-import * as THREE from "three";
-import { createRoot } from "react-dom/client";
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { Model } from "./../public/Model";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
 
-function Box(props: ThreeElements["mesh"]) {
-  const meshRef = useRef<THREE.Mesh>(null!);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (meshRef.current.rotation.x += 0.01));
-  return (
-    <mesh
-      {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
-      <sphereGeometry args={[0.1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-}
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-0.05, -0.15, 3.1]} />
-    <Box position={[0.05, -0.15, 3.1]} />
-    <Model position={[0, -1, 3]} />
-  </Canvas>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <App />
 );
