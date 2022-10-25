2a2f2a582430502943b1ed775fc0a4aac69e77ad
IdrisIsra <israpilov.idris@gmail.com>
2022-10-25 22:03:03 +0300

    added an AnimatedModel that does Capoeira

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Model } from "./Model";
+import { AnimatedModel } from "./AnimatedModel";
 import "./index.css";
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Suspense fallback={null}>
-          <Model position={[0, -1, 0]} />
+          <Model position={[-1, -1, 0]} />
+          <AnimatedModel position={[1, -1, 0]} />
           <OrbitControls />
         </Suspense>
       </Canvas>
