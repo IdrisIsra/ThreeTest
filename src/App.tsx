e33f338cc42b37995e2441e90c9b5668d29f739c
IdrisIsra <israpilov.idris@gmail.com>
2022-10-26 20:03:38 +0300

    added scroll area and scroll listener to tie animation to the scroll event

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import React, { Suspense, useRef, useState } from "react";
+import React, { createRef, Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { Model } from "./Model";
@@ -7,6 +7,14 @@ import { AnimatedModel } from "./AnimatedModel";
 import "./index.css";
 
 function App() {
+  const scrollArea = useRef<HTMLDivElement>(null);
+  const topRef = useRef<number>(0);
+  const onScroll = (e: any) => {
+    topRef.current = e.target.scrollTop;
+  };
+
+  useEffect(() => void onScroll({ target: scrollArea.current }), []);
+
   return (
     <div id="canvas-container" style={{ height: "100vh" }}>
       <Canvas>
@@ -14,10 +22,13 @@ function App() {
         <pointLight position={[10, 10, 10]} />
         <Suspense fallback={null}>
           <Model position={[-1, -1, 0]} />
-          <AnimatedModel position={[1, -1, 0]} />
-          <OrbitControls />
+          <AnimatedModel position={[1, -1, 0]} top={topRef} />
+          {/* <OrbitControls /> */}
         </Suspense>
       </Canvas>
+      <div ref={scrollArea} onScroll={onScroll} className="scrollArea">
+        <div style={{ height: `300vh` }} />
+      </div>
     </div>
   );
 }
